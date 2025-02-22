e3fb1ae334ae5e2e2c04db0a9def033c6522a77f
carlotasobral <up202403584@up.pt>
2025-02-22 15:08:54 +0000

    Fim do exercício 1 da ficha 2 de programação 2

--- Automovel.java
+++ Automovel.java
@@ -21,4 +21,62 @@ public class Automovel {
         this.cilindrada = cilindrada;
         totalAutomoveis++;
     }
+    public Automovel(String matricula, String marca) {
+        this.matricula = matricula;
+        this.marca = marca;
+        cilindrada = CILINDRADA_POR_OMISSAO;
+        totalAutomoveis++;
+    }
+
+    public Automovel(String marca, int cilindrada) {
+        this.marca = marca;
+        this.cilindrada = cilindrada;
+        matricula = MATRICULA_POR_OMISSAO;
+        totalAutomoveis++;
+    }
+
+    public String getMatricula() { //definir valores
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setMatricula(String matricula) { //alterar valores
+        this.matricula = matricula;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public String toString() {
+        return "O automóvel com matrícula " + matricula + " é um " + marca + "de " + cilindrada + " cc."; //" o automovel com a matricula %s é um %s e tem cilindrada de %d cc", matricula, marca, cilindrada
+    }
+
+    public int calcularDiferencaCilindrada(Automovel outroAutomovel) {
+        return Math.abs(cilindrada - outroAutomovel.getCilindrada());
+    }
+
+    public boolean isCilindradaMaior(Automovel outroAutomovel) {
+        return cilindrada > outroAutomovel.getCilindrada();
+
+    }
+
+    public boolean isCilindradaMaior (int cilindrada) {
+        return this.cilindrada > cilindrada;
+    }
+
+    public static int getTotalAutomoveis() {
+        return totalAutomoveis;
+    }
 }
